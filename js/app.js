c53ea8620f019ea48de75ed60e35f6a2a57c4e07
Sandra Czerkas <sandra.czerkas@gmail.com>
2018-04-17 22:02:38 -0400

    Added Player function definition

--- js/app.js
+++ js/app.js
@@ -25,12 +25,46 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 
+var Player = function(x,y) {
+    this.x = x;
+    this.y = y;
+    this.sprite = 'images/char-boy.png';
+};
+
+Player.prototype.update = function(){
+
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(direction){
+    alert(direction);
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+var enemy1 = new Enemy();
+var enemy2 = new Enemy();
+var enemy3 = new Enemy();
+var enemy4 = new Enemy();
+var enemy5 = new Enemy();
+var enemy6 = new Enemy();
 
+var allEnemies = [
+    enemy1,
+    enemy2,
+    enemy3,
+    enemy4,
+    enemy5,
+    enemy6
+];
+
+var player1 = new Player(200,300);
+var player = player1;
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
